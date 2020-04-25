ca55f3fcf8475106796e1ba88259dcb2b25d24fb
Sergi Simon Balcells <sergisiba@gmail.com>
2020-04-25 17:57:07 +0200

    Added product model skeleton
    XIULA AMB MI

---
+++ apps/product/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+# Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+    parent = models.ForeignKey("Tag", on_delete=models.CASCADE, null=True)
+
+
+class Product(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    name = models.CharField(max_length=78)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='img/product/', default=None, blank=True)
+
+
--- apps/users/models.py
+++ apps/users/models.py
@@ -33,7 +33,7 @@ class CustomUser(AbstractUser):
             ("SE", "Sweden"),
             ("IE", "Ireland"),
             ]
-    photo = models.ImageField(upload_to='img/', default=None, blank=True)
+    photo = models.ImageField(upload_to='img/user/', default=None, blank=True)
     description = models.TextField(blank=True)
     tin = models.CharField(max_length=32, unique=True)
     country = models.CharField(max_length=2, choices=STATES, default="DE", blank=True)
