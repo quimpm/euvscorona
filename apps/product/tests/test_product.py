a4dfb4422e6dad0eb23493d18cae64ffe237c42c
Oriol <oriolalascercos@gmail.com>
2020-04-25 18:39:35 +0200

    Added some unfinished tests

--- apps/product/tests/test_product.py
+++ apps/product/tests/test_product.py
@@ -1,6 +1,31 @@
 from django.test import TestCase
 from apps.product.models import Tag, Product
+from django.contrib.auth import get_user_model
 
 class CustomProductTest(TestCase):
 
     def test_simple_product(self):
+        parent_tag = Tag.objects.create(name='Alimentation', parent=None)
+        son_tag = Tag.objects.create(name='Potatoes', parent=parent_tag)
+        User = get_user_model()
+        user = User.objects.create_user(
+                username="wil",
+                email="will@email.com",
+                password='testpass123',
+                tin='12345X',
+                fiscal_address='St. Nothing',
+                phone='123456789',
+                lat=1.41,
+                lon=1.51,
+                description='Some weird description',
+                country='ES',
+        )
+        product = Product.objects.create(
+            creator=user,
+            tag = son_tag,
+            name = 'Patata Manhattan',
+            description = '45kg bag of potatoes',
+        )
+        self.assertEquals(user,product.creator)
+        #self.assertEquals(,)
+
--- apps/product/tests/test_tag.py
+++ apps/product/tests/test_tag.py
@@ -4,11 +4,12 @@ from apps.product.models import Tag
 class CustomTagTest(TestCase):
 
     def test_create_super_tag(self):
-        parent_tag = Tag.objects.create(name='Alimentació', parent=None)
-        son_tag = Tag.objects.create(name='Patates', parent=parent_tag)
+        parent_tag = Tag.objects.create(name='Alimentation', parent=None)
+        son_tag = Tag.objects.create(name='Potatoes', parent=parent_tag)
         self.assertEquals(parent_tag, son_tag.parent)
 
     def test_create_simple_Tag(self):
-        tag = Tag.objects.create(name='Alimentació', parent=None)
-        self.assertEquals(tag.name, 'Alimentació')
+        tag_name = 'Alimentation'
+        tag = Tag.objects.create(name=tag_name, parent=None)
+        self.assertEquals(tag.name, tag_name)
         self.assertIsNone(tag.parent)
