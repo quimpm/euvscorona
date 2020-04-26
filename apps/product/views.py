919198e4407d5b6fa263799ab1bb3b62db8152d5
Sergi Simon Balcells <sergisiba@gmail.com>
2020-04-26 17:38:52 +0200

    Solved high order functions, as there is no lenses in python :(

--- apps/product/views.py
+++ apps/product/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 from apps.product.models import Tag, Product
@@ -13,9 +14,15 @@ class NearbyProductsByTagList(LoginRequiredMixin, ListView):
     
     
     def get_queryset(self):
+        def get_key(func):
+            def a(product):
+                user = get_user_model().objects.get(pk=product.creator)
+                return(func(user))
+            return a
+
         tag = self.request.GET.get('q', 'all')
         tags = Tag.objects.get(name=tag)
         objs = Product.objects.filter(tag=tags)
         px = latlon.Point(self.request.user.lat, self.request.user.lon)
-        return sorted(objs, key=latlon.get_lanlonkey(px))
+        return sorted(objs, key=get_key(latlon.get_lanlonkey(px)))
 
